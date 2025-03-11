08a24bebcf161299498422310311fba286b04616
Italo <italoterto4@gmail.com>
2025-03-11 15:47:51 +0000

    [italo] yes?

--- skill-swap/src/main.tsx
+++ skill-swap/src/main.tsx
@@ -13,11 +13,11 @@ createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/CS4227-group-project/" element={<Navigate to="/admin" />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/profile/:id" element={<App />} />
-          <Route path="/message/:id" element={<App />} />
-          <Route path="/edit/:id" element={<AdminEditUser />} />
+          <Route path="/CS4227-group-project/" element={<Navigate to="/CS4227-group-project/admin" />} />
+          <Route path="/CS4227-group-project/admin" element={<AdminPage />} />
+          <Route path="/CS4227-group-project/profile/:id" element={<App />} />
+          <Route path="/CS4227-group-project/message/:id" element={<App />} />
+          <Route path="/CS4227-group-project/edit/:id" element={<AdminEditUser />} />
         </Routes>
       </Router>
     </Provider>
